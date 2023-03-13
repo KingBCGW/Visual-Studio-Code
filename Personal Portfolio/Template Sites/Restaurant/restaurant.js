1143d34e3b1a2719488211dac630173813485535
Brandon Wang <brandon.wang42@hotmail.com>
2023-03-13 12:23:05 -0400

    Restaurant Template

---
+++ Personal Portfolio/Template Sites/Restaurant/restaurant.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+
+        dots: true,
+        focusOnSelect: true,
+    });    
+  });
