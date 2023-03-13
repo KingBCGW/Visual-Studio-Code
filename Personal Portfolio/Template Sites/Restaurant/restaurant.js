e9ef4988a545a3c438eb9c3a5f91ec28d901c200
Jacky <jchoi53@mail.ccsf.edu>
2023-03-13 14:10:53 -0700

    fix slick carousel

--- Personal Portfolio/Template Sites/Restaurant/restaurant.js
+++ Personal Portfolio/Template Sites/Restaurant/restaurant.js
@@ -2,10 +2,9 @@ $(document).ready(function(){
     $('.autoplay').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
+        
         autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: true,
-
+        autoplaySpeed: 2000,    
         dots: true,
         focusOnSelect: true,
     });    
